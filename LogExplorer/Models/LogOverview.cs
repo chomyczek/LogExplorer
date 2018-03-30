f22e244c44a79c85f600c0a2ea3f43a9d689c5e3
Chomyczek <kaszubowski.adam@gmail.com>
2018-03-30 16:16:16 +0200

    add new log model

---
+++ LogExplorer/Models/LogOverview.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace LogExplorer.Models
+{
+    public class LogOverview
+    {
+        #region Public Properties
+
+        public string DirPath => Log.DirPath;
+
+        public string DurationString => Log.DurationString;
+
+        public List<Log> History { get; set; }
+
+        public bool IsSelected
+        {
+            get { return Log.IsSelected; }
+            set { Log.IsSelected = value; }
+        }
+
+        public string LogPath => Log.LogPath;
+
+        public string Name => Log.Name;
+
+        public string Result => Log.Result;
+
+        public Brush ResultColor => Log.ResultColor;
+
+        public string StartTimeString => Log.StartTimeString;
+
+        private Log Log => History[0];
+
+        #endregion
+    }
+}
