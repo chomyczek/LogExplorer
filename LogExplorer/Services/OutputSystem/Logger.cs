8a5641225872b5d29ebb0c4b0a5a84460a74ce74
Chomyczek <kaszubowski.adam@gmail.com>
2018-05-13 14:04:33 +0200

    add logger class

---
+++ LogExplorer/Services/OutputSystem/Logger.cs
@@ -0,0 +1,78 @@
+﻿// LogExplorer
+// Copyright(C) 2018
+// Author Adam Kaszubowski
+
+using System;
+using System.Linq.Expressions;
+
+namespace LogExplorer.Services.OutputSystem
+{
+	public class Logger
+	{
+		#region Static Fields
+
+		private static Logger instance;
+
+		private static Action propertyChange;
+
+		private string message;
+
+		private static object instanceLocker= new object();
+
+		private static object propertyChangeLocker = new object();
+
+		public string Message => this.message;
+
+		#endregion
+
+		#region Public Properties
+
+		public static Logger Instance
+		{
+			get
+			{
+				if (instance != null)
+				{
+					return instance;
+				}
+				lock (instanceLocker)
+				{
+					if (instance == null)
+					{
+						instance = new Logger();
+					}
+				}
+
+				return instance;
+			}
+		}
+		private Logger()
+		{
+			this.message = string.Empty;
+		}
+
+		public static void PrepareInstance(Action propertyChangeAction)
+		{
+			if (propertyChange != null)
+			{
+				return;
+			}
+			lock (propertyChangeLocker)
+			{
+				if (propertyChange == null)
+				{
+					propertyChange = propertyChangeAction;
+					propertyChange.Invoke();
+				}
+			}
+		}
+
+		public void AddMessage(string msg)
+		{
+			this.message = $"{msg}\n{this.message}";
+			propertyChange?.Invoke();
+		}
+
+		#endregion
+	}
+}
--- LogExplorer/ViewModels/LoggerViewModel.cs
+++ LogExplorer/ViewModels/LoggerViewModel.cs
@@ -1,14 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// LogExplorer
+// Copyright(C) 2018
+// Author Adam Kaszubowski
+
+#region Usings
+
+using LogExplorer.Services.OutputSystem;
 
 using MvvmCross.Core.ViewModels;
 
+#endregion
+
 namespace LogExplorer.ViewModels
 {
-	public class LoggerViewModel: MvxViewModel
+	public class LoggerViewModel : MvxViewModel
 	{
+		#region Fields
+
+		private readonly Logger logger;
+
+		#endregion
+
+		#region Constructors and Destructors
+
+		public LoggerViewModel()
+		{
+			Logger.PrepareInstance(() => { this.RaisePropertyChanged(() => this.LoggerBox); });
+			this.logger = Logger.Instance;
+		}
+
+		#endregion
+
+		#region Public Properties
+
+		public string LoggerBox
+		{
+			get
+			{
+				return this.logger.Message;
+			}
+			//Empty set to avoid writing to logger
+			set
+			{
+			}
+		}
+
+		#endregion
 	}
 }
