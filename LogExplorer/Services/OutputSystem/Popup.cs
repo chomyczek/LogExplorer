1fbe39ca413f78dd268b3195cd81f9bb77718f4a
Chomyczek <kaszubowski.adam@gmail.com>
2018-05-13 14:38:57 +0200

    add popup class

---
+++ LogExplorer/Services/OutputSystem/Popup.cs
@@ -0,0 +1,34 @@
+﻿// LogExplorer
+// Copyright(C) 2018
+// Author Adam Kaszubowski
+
+#region Usings
+
+using System.Windows;
+
+#endregion
+
+namespace LogExplorer.Services.OutputSystem
+{
+	public class Popup
+	{
+		#region Public Methods and Operators
+
+		public static void ShowError(string message)
+		{
+			MessageBox.Show(message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+		}
+
+		public static void ShowInfo(string message)
+		{
+			MessageBox.Show(message, "Info!", MessageBoxButton.OK, MessageBoxImage.Information);
+		}
+
+		public static void ShowWarning(string message)
+		{
+			MessageBox.Show(message, "Warning!", MessageBoxButton.OK, MessageBoxImage.Warning);
+		}
+
+		#endregion
+	}
+}
