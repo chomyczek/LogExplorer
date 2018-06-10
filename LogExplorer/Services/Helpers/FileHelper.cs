344e5226251e4119b88dc014bc58362b5606a9e6
Chomyczek <kaszubowski.adam@gmail.com>
2018-06-10 16:41:24 +0200

    commit missing changes

--- LogExplorer/Services/Helpers/FileHelper.cs
+++ LogExplorer/Services/Helpers/FileHelper.cs
@@ -23,7 +23,12 @@ namespace LogExplorer.Services.Helpers
 {
 	public static class FileHelper
 	{
+		#region Static Fields
+
 		private static readonly Logger Logger = Logger.Instance;
+
+		#endregion
+
 		#region Public Methods and Operators
 
 		public static string CombinePaths(string p1, string p2)
@@ -43,17 +48,31 @@ namespace LogExplorer.Services.Helpers
 				{
 					p2 = "null";
 				}
-				Logger.AddDetailMessage(Messages.GetCombineProblem(p1,p2,e.Message));
-				
+				Logger.AddDetailMessage(Messages.GetCombineProblem(p1, p2, e.Message));
+
 				return p1;
 			}
 			return combination;
 		}
 
-		public static string GetParent(string path)
+		public static void CopyFile(string source, string destination)
 		{
-			var parent = Directory.GetParent(path);
-			return parent.FullName;
+			File.Copy(source, destination, true);
+		}
+
+		public static bool CreateDir(string path)
+		{
+			try
+			{
+				Directory.CreateDirectory(path);
+			}
+			catch (Exception e)
+			{
+				Popup.ShowError(Messages.GetCantCreateDir(e.Message));
+				return false;
+			}
+			Logger.AddDetailMessage(Messages.GetCreateDirSuccess(path));
+			return true;
 		}
 
 		public static bool Delete(string path)
@@ -81,7 +100,7 @@ namespace LogExplorer.Services.Helpers
 			{
 				if (PathExist(path))
 				{
-					FileHelper.StartProcess(path);
+					StartProcess(path);
 				}
 				Logger.AddMessage(Messages.GetDeletDirException(e.Message));
 				return false;
@@ -90,53 +109,40 @@ namespace LogExplorer.Services.Helpers
 			return true;
 		}
 
-	    public static string[] GetFiles(string path, string extension = "*")
-	    {
-	        if (!PathExist(path))
-	        {
-	            return new string[0];
-	        }
-
-            var files = Directory.GetFiles(path, $"*.{extension}");
-
-	        return files;
-	    }
-		
-        public static string GetFileName(string path)
-        {
-            return Path.GetFileNameWithoutExtension(path);
-        }
-
-        public static void CopyFile(string source, string destination)
+		public static bool EmptyDir(string path)
 		{
-			File.Copy(source, destination, true);
+			var containsFiles = GetFiles(path).Any();
+			var containsDirs = Directory.GetDirectories(path).Any();
+			return !containsDirs && !containsFiles;
 		}
 
-		public static bool CreateDir(string path)
+		public static bool FileExist(string path)
 		{
-			try
+			if (!string.IsNullOrEmpty(path)
+			    && File.Exists(path))
 			{
-				Directory.CreateDirectory(path);
+				return true;
 			}
-			catch (Exception e)
-			{
-				Popup.ShowError(Messages.GetCantCreateDir(e.Message));
-				return false;
-			}
-			Logger.AddDetailMessage(Messages.GetCreateDirSuccess(path));
-			return true;
-		}
-        
-        public static bool FileExist(string path)
-		{
-	        if (!string.IsNullOrEmpty(path)
-	            && File.Exists(path))
-	        {
-		        return true;
-	        }
 
 			Logger.AddDetailMessage(Messages.GetFileNotExist(path));
-	        return false;
+			return false;
+		}
+
+		public static string GetFileName(string path)
+		{
+			return Path.GetFileNameWithoutExtension(path);
+		}
+
+		public static string[] GetFiles(string path, string extension = "*")
+		{
+			if (!PathExist(path))
+			{
+				return new string[0];
+			}
+
+			var files = Directory.GetFiles(path, $"*.{extension}");
+
+			return files;
 		}
 
 		public static string GetLocalPath(string fileName)
@@ -150,10 +156,16 @@ namespace LogExplorer.Services.Helpers
 			return Path.Combine(fullPath, fileName);
 		}
 
+		public static string GetParent(string path)
+		{
+			var parent = Directory.GetParent(path);
+			return parent.FullName;
+		}
+
 		public static bool PathExist(string path)
 		{
 			if (!string.IsNullOrEmpty(path)
-				&& Directory.Exists(path))
+			    && Directory.Exists(path))
 			{
 				return true;
 			}
@@ -175,79 +187,45 @@ namespace LogExplorer.Services.Helpers
 			return directory;
 		}
 
-	    public static string SelectFile(string path, string extension, string extensionDescription)
-	    {
-	        var newPath = path;
-
-	        using (
-	            var openFileDialog = new OpenFileDialog
-	            {
-	                InitialDirectory =
-	                    string.IsNullOrEmpty(path) ? @"c:\" : Path.GetDirectoryName(path),
-	                Filter = $"{extensionDescription} (*.{extension})|*.{extension}",
-	                RestoreDirectory = true,
-	                Multiselect = false
-	            })
-	        {
-	            if (openFileDialog.ShowDialog() == DialogResult.OK)
-	            {
-	                newPath = openFileDialog.FileName;
-	            }
-	        }
-
-	        return newPath;
-	    }
-
-        public static bool StartProcess(string path)
+		public static string SelectFile(string path, string extension, string extensionDescription)
 		{
-			var attr = File.GetAttributes(path);
-			
-	        if ((attr & FileAttributes.Directory) == FileAttributes.Directory
-	            && !PathExist(path))
-	        {
-				return false;
-	        }
-	        if (!FileExist(path))
-	        {
-		        return false;
-	        }
+			var newPath = path;
 
-	        var process = new Process { StartInfo = new ProcessStartInfo(path) };
-			
-			try
-	        {
-		        if (process.Start())
-		        {
-			        return true;
-		        }
-				Popup.ShowWarning(Messages.GetProcessNotStart(path));
-				return false;
+			using (
+				var openFileDialog = new OpenFileDialog
+				                     {
+					                     InitialDirectory =
+						                     string.IsNullOrEmpty(path) ? @"c:\" : Path.GetDirectoryName(path),
+					                     Filter = $"{extensionDescription} (*.{extension})|*.{extension}",
+					                     RestoreDirectory = true,
+					                     Multiselect = false
+				                     })
+			{
+				if (openFileDialog.ShowDialog() == DialogResult.OK)
+				{
+					newPath = openFileDialog.FileName;
+				}
+			}
 
-	        }
-	        catch (Exception e)
-	        {
-				Popup.ShowError(Messages.GetProcessException(path, e.Message));
-		        return false;
-	        }
-			
+			return newPath;
 		}
 
-	    public static bool StartCmdProcess(string command, string workingDir, bool hidden)
-	    {
-	        if (string.IsNullOrEmpty(command))
-	        {
-	            return false;
-	        }
+		public static bool StartCmdProcess(string command, string workingDir, bool hidden)
+		{
+			if (string.IsNullOrEmpty(command))
+			{
+				return false;
+			}
 
-	        var process = new Process();
-	        var startInfo = new ProcessStartInfo
-	        {
-	            WindowStyle = hidden ? ProcessWindowStyle.Hidden : ProcessWindowStyle.Normal,
-	            WorkingDirectory = workingDir,
-	            FileName = "cmd.exe",
-	            Arguments = $@"/C {command}"
-	        };
-	        process.StartInfo = startInfo;
+			var process = new Process();
+			var startInfo = new ProcessStartInfo
+			                {
+				                WindowStyle = hidden ? ProcessWindowStyle.Hidden : ProcessWindowStyle.Normal,
+				                WorkingDirectory = workingDir,
+				                FileName = "cmd.exe",
+				                Arguments = $@"/C {command}"
+			                };
+			process.StartInfo = startInfo;
 
 			try
 			{
@@ -257,23 +235,46 @@ namespace LogExplorer.Services.Helpers
 				}
 				Popup.ShowWarning(Messages.GetProcessNotStart(command));
 				return false;
-
 			}
 			catch (Exception e)
 			{
 				Popup.ShowError(Messages.GetProcessException(command, e.Message));
 				return false;
 			}
-			
-	    }
-
-        #endregion
-
-		public static bool EmptyDir(string path)
-		{
-			var containsFiles = GetFiles(path).Any();
-			var containsDirs = Directory.GetDirectories(path).Any();
-			return !containsDirs && !containsFiles;
 		}
+
+		public static bool StartProcess(string path)
+		{
+			var attr = File.GetAttributes(path);
+
+			if ((attr & FileAttributes.Directory) == FileAttributes.Directory
+			    && !PathExist(path))
+			{
+				return false;
+			}
+			if (!FileExist(path))
+			{
+				return false;
+			}
+
+			var process = new Process { StartInfo = new ProcessStartInfo(path) };
+
+			try
+			{
+				if (process.Start())
+				{
+					return true;
+				}
+				Popup.ShowWarning(Messages.GetProcessNotStart(path));
+				return false;
+			}
+			catch (Exception e)
+			{
+				Popup.ShowError(Messages.GetProcessException(path, e.Message));
+				return false;
+			}
+		}
+
+		#endregion
 	}
 }
